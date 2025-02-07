8e0057ad7267d25c756b2772cf154a6f19c7b06e
Angela296020 <angelamoras296@gmail.com>
2025-02-07 11:04:58 +0530

    Add files via upload

---
+++ Conditional  Statements.py
@@ -0,0 +1,34 @@
+# age = 21
+# if age >=18:
+#     print("correct age")
+# else:
+#     print("wrong age")
+
+# marks = 90
+# if marks >=75:
+#     print("passed with distinction")
+# elif marks<=45 and marks>=70:
+#     print("passed with first class")
+# elif marks<=28 and marks>=45:
+#     print("passed with second class")
+# elif marks<=45 and marks>=0:
+#     print("passed with third class")
+# else:
+#     print("failed")
+
+# signal_light = "blue"
+# if signal_light == "green":
+#     print("Go")
+# elifsignal_light == "yellow":
+#     print("ready")
+# else:
+#     print("stop")
+
+# if signal_light == "green":
+#     print("Go")
+# elifsignal_light == "yellow":
+#     print("ready")
+# elifsignal_light == "red":
+#     print("stop")
+# else:
+#     print("none")
